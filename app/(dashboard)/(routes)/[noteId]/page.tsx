d9f9237c39b1b81b75b9cb19a01cb74d89022613
Drake669 <eyatompoya@gmail.com>
2024-01-14 17:32:06 +0000

    feat: updating note title

--- app/(dashboard)/(routes)/[noteId]/page.tsx
+++ app/(dashboard)/(routes)/[noteId]/page.tsx
@@ -1,5 +1,59 @@
+"use client";
+
+import TitleEditForm from "@/components/TitleEditForm";
+import { Note } from "@prisma/client";
+import axios from "axios";
+import { Loader2, Pencil } from "lucide-react";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+
 const Note = ({ params }: { params: { noteId: string } }) => {
-  return <div className="p-6">Hello world</div>;
+  const [currentNote, setCurrentNote] = useState<Note | null>(null);
+  const [titleEdit, setTitleEdit] = useState(false);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const getNote = async () => {
+      try {
+        const res = await axios.get(`/api/notes/${params.noteId}`);
+        setCurrentNote(res.data);
+        setLoading(true);
+      } catch (error) {
+        if (axios.isAxiosError(error)) {
+          toast.error(error.response?.data || "An error occured");
+        } else {
+          toast.error("An error occured");
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+    getNote();
+  }, [params.noteId]);
+  if (loading)
+    return (
+      <div className="relative flex items-center justify-center h-full">
+        <div className="absolute top-0 left-50 flex items-center justify-center h-full">
+          <Loader2 className="h-10 w-10 animate-spin transition" />
+        </div>
+      </div>
+    );
+  return (
+    <div className="p-6">
+      {titleEdit ? (
+        <TitleEditForm note={currentNote} setTitleEdit={setTitleEdit} />
+      ) : (
+        <div className="flex items-center">
+          <div className=" font-bold text-3xl">{currentNote?.title}</div>
+          <Pencil
+            className="ml-2 w-5 h-5 hover:cursor-pointer hover:opacity-75"
+            onClick={() => {
+              setTitleEdit(true);
+            }}
+          />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Note;
---
+++ components/TitleEditForm.tsx
@@ -0,0 +1,100 @@
+import React, { useEffect } from "react";
+import * as z from "zod";
+import axios from "axios";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { Loader2 } from "lucide-react";
+import { Note } from "@prisma/client";
+
+const formSchema = z.object({
+  title: z.string().min(2, {
+    message: "Title must be at least 2 characters.",
+  }),
+});
+
+const TitleEditForm = ({
+  note,
+  setTitleEdit,
+}: {
+  note: Note | null;
+  setTitleEdit: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
+  const router = useRouter();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: note?.title || "",
+    },
+  });
+
+  const { isSubmitting } = form.formState;
+
+  // 2. Define a submit handler.
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      const response = await axios.post("/api/notes", values);
+      toast.success("Note updated successfully");
+      router.refresh();
+      form.reset();
+      setTitleEdit(false);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        const errorMessage = error.response?.data || "An error occurred";
+        toast.error(errorMessage);
+      } else {
+        const errorMessage = (error as Error).message || "An error occurred";
+        toast.error(errorMessage);
+      }
+    }
+  }
+  return (
+    <>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className=" flex items-center"
+        >
+          <div>
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      placeholder="Enter a title for the note"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="ml-2">
+            <Button type="submit" disabled={isSubmitting}>
+              {isSubmitting && (
+                <Loader2 className="w-4 h-4 animate-spin transition mr-2" />
+              )}
+              Save
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </>
+  );
+};
+
+export default TitleEditForm;
