6b2142f853db81b44fceb624dda45b17c3b61bce
Drake669 <eyatompoya@gmail.com>
2024-01-14 16:50:58 +0000

    feat: updating and getting of notes

--- app/api/notes/[noteId]/route.ts
+++ app/api/notes/[noteId]/route.ts
@@ -1,6 +1,46 @@
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
+
+export async function GET(req:Request, {params}: {params: {noteId: string}}) {
+    try {
+        const note = await db.note.findUnique({
+            where: {
+                id: params.noteId
+            }
+        })
+        if(!note) return new NextResponse("Note not found", {status: 404})
+        return NextResponse.json(note)
+        } catch (error) {
+        console.log("UPDATE NOTE ERROR", error)
+        return new NextResponse("Internal Server Error", {status: 500})
+    }
+}
+
+export async function PATCH(req:Request, {params}: {params: {noteId: string}}) {
+    try {
+        const note = await db.note.findUnique({
+            where: {
+                id: params.noteId
+            }
+        })
+        if(!note) return new NextResponse("Note not found", {status: 404})
+        const values = await req.json()
+        const newNote = await db.note.update({
+            where: {
+                id: note.id
+            },
+            data: {
+                ...values
+            }
+        })
+        return NextResponse.json(newNote)
+        } catch (error) {
+        console.log("UPDATE NOTE ERROR", error)
+        return new NextResponse("Internal Server Error", {status: 500})
+    }
+}
+
 export async function DELETE(req:Request, {params}: {params: {noteId: string}}) {
     try {
         const note = await db.note.findUnique({
